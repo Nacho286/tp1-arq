695397f0191d475443369798d29604ed7850bcc9
florencia <fsavoretti@afip.gob.ar>
2019-05-18 09:43:29 -0300

    Autenticacion preliminar

--- grails-app/init/tp1/BootStrap.groovy
+++ grails-app/init/tp1/BootStrap.groovy
@@ -1,8 +1,14 @@
 package tp1
 
+import arquiweb.tp1.auth.UserDataService
+
 class BootStrap {
+    def authenticateService
 
     def init = { servletContext ->
+        println 'Initializing users..'
+        //UserDataService.save('kermit',  'frog', true, false, false, false)
+        //new User(username: 'kermit', password: 'frog').save()
     }
     def destroy = {
     }
