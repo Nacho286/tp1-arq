f4f9bb276c6ea86544b49cade2a9c5a74a3d142b
florencia <fsavoretti@afip.gob.ar>
2019-06-08 00:33:50 -0300

    creamos los datos solo en ambiente DEVELOPMENT

--- grails-app/init/arquiweb/tp1/BootStrap.groovy
+++ grails-app/init/arquiweb/tp1/BootStrap.groovy
@@ -7,44 +7,47 @@ class BootStrap {
     def authenticateService
 
     def init = { servletContext ->
-        println 'Initializing users..'
 
-        def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
-        def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
+        if (Environment.current == Environment.DEVELOPMENT) {
+            println 'Initializing users..'
 
-        def nachoUser = new User(username: 'nacho', password: 'password')
-        def jcUser = new User(username: 'juancruz', password: 'password')
-        def florUser = new User(username: 'flor', password: 'password')
+            def adminRole = new Role(authority: 'ROLE_ADMIN').save(flush: true)
+            def userRole = new Role(authority: 'ROLE_USER').save(flush: true)
 
-        nachoUser.save(flush: true, failOnError: true)
-        jcUser.save(flush: true, failOnError: true)
-        florUser.save(flush: true, failOnError: true)
+            def nachoUser = new User(username: 'nacho', password: 'password')
+            def jcUser = new User(username: 'juancruz', password: 'password')
+            def florUser = new User(username: 'flor', password: 'password')
 
-        UserRole.create nachoUser, adminRole, true
-        UserRole.create jcUser, userRole, true
-        UserRole.create florUser, adminRole, true
+            nachoUser.save(flush: true, failOnError: true)
+            jcUser.save(flush: true, failOnError: true)
+            florUser.save(flush: true, failOnError: true)
 
-        println 'Initializing categories...'
+            UserRole.create nachoUser, adminRole, true
+            UserRole.create jcUser, userRole, true
+            UserRole.create florUser, adminRole, true
 
-        def categoryNames = ['Restaurante', 'Farmacia', 'Supermercado', 'Dietetica', 'Carniceria']
+            println 'Initializing categories...'
 
-        for (c in categoryNames) {
+            def categoryNames = ['Restaurante', 'Farmacia', 'Supermercado', 'Dietetica', 'Carniceria']
 
-            def cat = new Category(
-                    ['name'  : c,
-                     approved: true]
-            )
-            cat.save(flush: true, failOnError: true)
+            for (c in categoryNames) {
 
-            for (i in 1..5) {
-                def markerPrueba = new Marker(
-                        [title      : c[0..4] + '_' + i,
-                         latitude   : -34.603722,
-                         longitude  : -58.381592,
-                         description: c + ' ' + i,
-                         visible    : true])
-                markerPrueba.setCategory(cat)
-                markerPrueba.save(flush: true, failOnError: true)
+                def cat = new Category(
+                        ['name'  : c,
+                         approved: true]
+                )
+                cat.save(flush: true, failOnError: true)
+
+                for (i in 1..5) {
+                    def markerPrueba = new Marker(
+                            [title      : c[0..4] + '_' + i,
+                             latitude   : -34.603722,
+                             longitude  : -58.381592,
+                             description: c + ' ' + i,
+                             visible    : true])
+                    markerPrueba.setCategory(cat)
+                    markerPrueba.save(flush: true, failOnError: true)
+                }
             }
         }
     }
