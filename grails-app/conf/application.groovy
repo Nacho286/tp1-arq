aafb066cd1300be3ca4b3e565fdce9ae75aad28d
Juan Cruz Basso <juan.basso@mercadolibre.com>
2019-06-10 21:03:46 -0300

    Fixing login

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -6,31 +6,33 @@ grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'arquiweb.tp1.a
 grails.plugin.springsecurity.authority.className = 'arquiweb.tp1.auth.Role'
 grails.plugin.springsecurity.logout.postOnly = false
 grails.plugin.springsecurity.controllerAnnotations.staticRules = [
-	[pattern: '/markers', 		 access: ['permitAll']],
-	[pattern: '/categories', 	 access: ['permitAll']],
-	[pattern: '/',               access: ['permitAll']],
-	[pattern: '/error',          access: ['permitAll']],
-	[pattern: '/index',          access: ['permitAll']],
-	[pattern: '/index.gsp',      access: ['permitAll']],
-	[pattern: '/shutdown',       access: ['permitAll']],
-	[pattern: '/assets/**',      access: ['permitAll']],
-	[pattern: '/**/js/**',       access: ['permitAll']],
-	[pattern: '/**/css/**',      access: ['permitAll']],
-	[pattern: '/**/images/**',   access: ['permitAll']],
-	[pattern: '/**/favicon.ico', access: ['permitAll']],
-	[pattern: '/dbconsole/*',	 access: ['permitAll']], /*Dejo esto para poder tocar la BD*/
-	[pattern: '/marker/markers', access: ['permitAll']],
-	[pattern: '/marker/*', 		 access: ['ROLE_USER','ROLE_ADMIN']],
-	[pattern: '/category/*', 	 access: ['ROLE_ADMIN']]
+		[pattern: '/markers', 		 access: ['permitAll']],
+		[pattern: '/categories', 	 access: ['permitAll']],
+		[pattern: '/',               access: ['permitAll']],
+		[pattern: '/error',          access: ['permitAll']],
+		[pattern: '/index',          access: ['permitAll']],
+		[pattern: '/index.gsp',      access: ['permitAll']],
+		[pattern: '/shutdown',       access: ['permitAll']],
+		[pattern: '/assets/**',      access: ['permitAll']],
+		[pattern: '/**/js/**',       access: ['permitAll']],
+		[pattern: '/**/css/**',      access: ['permitAll']],
+		[pattern: '/**/images/**',   access: ['permitAll']],
+		[pattern: '/**/favicon.ico', access: ['permitAll']],
+		[pattern: '/dbconsole/*',	 access: ['permitAll']], /*Dejo esto para poder tocar la BD*/
+		[pattern: '/marker/markers', access: ['permitAll']],
+		[pattern: '/marker/*', 		 access: ['ROLE_USER','ROLE_ADMIN']],
+		[pattern: '/category/*', 	 access: ['ROLE_ADMIN']]
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
-	[pattern: '/assets/**',      filters: 'none'],
-	[pattern: '/**/js/**',       filters: 'none'],
-	[pattern: '/**/css/**',      filters: 'none'],
-	[pattern: '/**/images/**',   filters: 'none'],
-	[pattern: '/**/favicon.ico', filters: 'none'],
-	[pattern: '/marker/*',       filters: 'JOINED_FILTERS'],
-	[pattern: '/category/*',     filters: 'JOINED_FILTERS']
+		[pattern: '/assets/**',      filters: 'none'],
+		[pattern: '/**/js/**',       filters: 'none'],
+		[pattern: '/**/css/**',      filters: 'none'],
+		[pattern: '/**/images/**',   filters: 'none'],
+		[pattern: '/**/favicon.ico', filters: 'none'],
+		[pattern: '/markers', 		 filters: 'none'],
+		[pattern: '/categories', 	 filters: 'none'],
+		[pattern: '/search*', 	 	 filters: 'none'],
+		[pattern: '/**',             filters: 'JOINED_FILTERS']
 ]
 
